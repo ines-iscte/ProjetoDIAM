4e96a5476dbca0fdebe8b18ad82e8708f64b5f5c
anamoreira-iscte <amsrm@iscte-iul.pt>
2023-05-09 19:25:40 +0100

    chatices

--- ProjDiam/movies/views.py
+++ ProjDiam/movies/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.db.models import Q
 from .models import Movie
 
 app_name = 'movies'
@@ -14,4 +14,11 @@ def index (request):
 def movie_list(request):
     movies = Movie.objects.all()
     return render(request, 'movies/movielist.html', {'movies': movies})
+
+
+def search_movies(request):
+    query = request.GET.get('query', '')
+    movies = Movie.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
+    context = {'movies': movies, 'query': query}
+    return render(request, 'search_results.html', context)
 
